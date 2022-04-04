ea00a8a66aa996d4587a10939fe6cf22af2154ad
Nusan Rana <nusanrana@Nusans-MacBook-Pro.local>
2022-04-04 16:42:12 -0700

    added users/lobby page

--- my-app/routes/users.js
+++ my-app/routes/users.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/lobby', function(req, res, next) {
+  res.render('lobby');
+});
 module.exports = router;
