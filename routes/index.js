812993bdef7ec757e5d28e77923c3a6e65504b3a
Juan408AR <aguirre.juanmp15@gmail.com>
2022-04-11 09:29:38 -0700

    Converted file to pug. The path looks different than before in that the program starts look for the stylesheet beginning at /stylesheet

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/register', function(req, res, next) {
   res.render('register');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 module.exports = router;
