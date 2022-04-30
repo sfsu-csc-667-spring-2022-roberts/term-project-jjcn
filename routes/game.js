a54eaf70b1e8c7c51e19d3b5ca9d0996af5f5e60
Chi Tran <ttranpham@mail.sfsu.edu>
2022-04-29 23:56:59 -0700

    added game page and responsive decks

--- routes/game.js
+++ routes/game.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    res.render('game', { title: 'Game Board' });
+});
+
+router.get('/board', (req,res) => {
     res.render('unoBoard', { title: 'Game Board' });
 });
 
