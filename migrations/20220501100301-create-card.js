e4678801d4a37d7fe6719bb203746d24cd038299
nushan rana <ad.nusan@gmail.com>
2022-05-01 03:10:09 -0700

    added card db, still figuring out db :(

--- migrations/20220501100301-create-card.js
+++ migrations/20220501100301-create-card.js
@@ -1,34 +1,33 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Cards', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firstName: {
+      cardColor: {
         type: Sequelize.STRING
       },
-      lastName: {
-        type: Sequelize.STRING
+      cardNum: {
+        type: Sequelize.INTEGER
       },
-      email: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        unique: true,
-        validate: {
-          isEmail: true
-        }
-
+      cardAction: {
+        type: Sequelize.STRING
       },
       createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
         type: Sequelize.DATE
       }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Cards');
   }
 };
