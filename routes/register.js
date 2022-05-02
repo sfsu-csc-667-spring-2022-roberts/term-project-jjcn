890b8729b051a0d1a7b0cbc65eb2e830b56588ec
nushan rana <ad.nusan@gmail.com>
2022-05-01 17:41:13 -0700

    trying to insert data into db from registration form.... getting error

--- routes/register.js
+++ routes/register.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var router = express.Router();
-
+const userdb = require('./../models/user');
 /* GET login page. */
 router.get('/', function(req, res, next) {
     res.render('register', { title: 'Register' });
 });
 
 const bodyParser = require('body-parser');
+const { response } = require('../app');
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post('/', function(req, res, next){
-    var email = req.body.email;
+    var emails = req.body.email;
     var username = req.body.username;
-    var password = req.body.password;
-    console.log(email, username, password);
-    res.send('account info has been received');
-})
+    var passwords = req.body.password;
+   // console.log(emails, username, passwords);
+    //res.send('account info has been received');
 
-module.exports = router;
+    //inserting user registration info into user database
+    userdb.create({ //getting TypeError: userdb.create is not a function
+        //userID: 123456,
+        firstName: username,
+        lastName: "lastname",
+        email: emails,
+        password: passwords,
+        createdAt: new Date()
+    }).then((userdb) => {
+        res.json(userdb);
+    }, (errors) =>{
+        res.json(errors);
+    });
+
+
+})
+module.exports = router, userdb;
