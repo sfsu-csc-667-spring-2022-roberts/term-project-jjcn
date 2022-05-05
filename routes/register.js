0e8d9a03bdd6ac1e3a3bf0f582e45af70714e3d1
nushan rana <ad.nusan@gmail.com>
2022-05-04 21:41:30 -0700

    connecting register to db, DOESNT WORK YET

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const userdb = require('./../models/user');
+const userdb = require('../db/index');
+
 /* GET login page. */
 router.get('/', function(req, res, next) {
     res.render('register', { title: 'Register' });
@@ -10,26 +11,31 @@ const bodyParser = require('body-parser');
 const { response } = require('../app');
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.post('/', function(req, res, next){
+router.post('/',async function(req, res, next){
+
     var emails = req.body.email;
     var username = req.body.username;
     var passwords = req.body.password;
    // console.log(emails, username, passwords);
     //res.send('account info has been received');
 
+
     //inserting user registration info into user database
-    userdb.create({ //getting TypeError: userdb.create is not a function
-        //userID: 123456,
-        firstName: username,
-        lastName: "lastname",
-        email: emails,
-        password: passwords,
-        createdAt: new Date()
-    }).then((userdb) => {
-        res.json(userdb);
-    }, (errors) =>{
-        res.json(errors);
-    });
+    const todayDate = new Date();
+    userdb.query(`INSERT INTO "User" (userID, firstName, lastName, email,createdAt, updatedAt) 
+	VALUES (918, 'Nusan', 'Adhikari','nusan14@gmail.com',${Date.now()},${Date.now()} ) `);
+    // return await userdb.create({ //getting TypeError: userdb.create is not a function
+    //     //userID: 123456,
+    //     firstName: username,
+    //     lastName: "lastname",
+    //     email: emails,
+    //     password: passwords,
+    //     createdAt: new Date()
+    // }).then((userdb) => {
+    //     res.json(userdb);
+    // }, (errors) =>{
+    //     res.json(errors);
+    // });
 
 
 })
