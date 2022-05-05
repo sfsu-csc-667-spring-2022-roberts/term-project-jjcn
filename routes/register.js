9cc4ca9e08064f43f444890b4c136f1ad94e5824
nushan rana <ad.nusan@gmail.com>
2022-05-04 23:58:38 -0700

    registration works now! it adds user info to db, need to excrypt password, ALSO need to make password type to STRING in db

--- routes/register.js
+++ routes/register.js
@@ -1,42 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const userdb = require('../db/index');
+const userdb = require("../db/index");
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
-    res.render('register', { title: 'Register' });
+router.get("/", function (req, res, next) {
+  res.render("register", { title: "Register" });
 });
 
-const bodyParser = require('body-parser');
-const { response } = require('../app');
+const bodyParser = require("body-parser");
+const { response } = require("../app");
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.post('/',async function(req, res, next){
+router.post("/", async function (req, res, next) {
+  var username = req.body.username;
+  var lastN = "last name"; //we dont have last name in register form so adding here, i'll remove lastname attrubute from db table
+  var emails = req.body.email;
+  var passwords = req.body.password; //password only accepts integer, need to make it integer
+  //var success = "account info has been received $1"
+  // console.log(emails, username, passwords);
 
-    var emails = req.body.email;
-    var username = req.body.username;
-    var passwords = req.body.password;
-   // console.log(emails, username, passwords);
-    //res.send('account info has been received');
-
-
-    //inserting user registration info into user database
-    const todayDate = new Date();
-    userdb.query(`INSERT INTO "User" (userID, firstName, lastName, email,createdAt, updatedAt) 
-	VALUES (918, 'Nusan', 'Adhikari','nusan14@gmail.com',${Date.now()},${Date.now()} ) `);
-    // return await userdb.create({ //getting TypeError: userdb.create is not a function
-    //     //userID: 123456,
-    //     firstName: username,
-    //     lastName: "lastname",
-    //     email: emails,
-    //     password: passwords,
-    //     createdAt: new Date()
-    // }).then((userdb) => {
-    //     res.json(userdb);
-    // }, (errors) =>{
-    //     res.json(errors);
-    // });
-
-
-})
-module.exports = router, userdb;
+  //inserting user registration info into user database
+  const todayDate = new Date();
+  userdb.query(
+    `INSERT INTO "Users" ("firstName","lastName","email","password","createdAt") 
+	VALUES ( $1,$2,$3,$4,$5) returning *`,
+    [username, lastN, emails, passwords, todayDate]
+  ).then,
+    res.send("account info has been received: " + username);
+});
+(module.exports = router), userdb;
