b060e0249e24f0d5199c04c5fdb93f211a5a0c7d
Chi Tran <ttranpham@mail.sfsu.edu>
2022-05-12 13:40:18 -0700

    added card images as div tags

--- public/src/cards.js
+++ public/src/cards.js
@@ -15,84 +15,84 @@ const r1 = {
     id: 'r1',
     value: 1,
     color: 'red',
-    image: '.unoCard.red.card1'
+    image: 'unoCard red card1'
 }
 
 const r2 = {
     id: 'r2',
     value: 2,
     color: 'red',
-    image: '.unoCard.red.card2'
+    image: 'unoCard red card2'
 }
 
 const r3 = {
     id: 'r3',
     value: 3,
     color: 'red',
-    image: '.unoCard.red.card3'
+    image: 'unoCard red card3'
 }
 
 const r4 = {
     id: 'r4',
     value: 4,
     color: 'red',
-    image: '.unoCard.red.card4'
+    image: 'unoCard red card4'
 }
 
 const r5 = {
     id: 'r5',
     value: 5,
     color: 'red',
-    image: '.unoCard.red.card5'
+    image: 'unoCard red card5'
 }
 
 const r6 = {
     id: 'r6',
     value: 6,
     color: 'red',
-    image: '.unoCard.red.card6'
+    image: 'unoCard red card6'
 }
 
 const r7 = {
     id: 'r7',
     value: 7,
     color: 'red',
-    image: '.unoCard.red.card7'
+    image: 'unoCard red card7'
 }
 
 const r8 = {
     id: 'r8',
     value: 8,
     color: 'red',
-    image: '.unoCard.red.card8'
+    image: 'unoCard red card8'
 }
 
 const r9 = {
     id: 'r9',
     value: 9,
     color: 'red',
-    image: '.unoCard.red.card9'
+    image: 'unoCard red card9'
 }
 
 const r_rev = {
     id: 'rrev',
     value: 11,
     color: 'red',
-    image: '.unoCard.red.reverse'
+    image: 'unoCard red reverse'
 }
 
 const r_draw2 = {
     id: 'rdraw2',
     value: 12,
     color: 'red',
-    image: '.unoCard.red.draw2'
+    image: 'unoCard red draw2'
 }
 
 const r_skip = {
     id: 'rskip',
     value: 13,
     color: 'red',
-    image: '.unoCard.red.skip'
+    image: 'unoCard red skip'
 }
 
 //Blue card declarations
@@ -101,84 +101,84 @@ const b1 = {
     id: 'b1',
     value: 1,
     color: 'blue',
-    image: '.unoCard.blue.card1'
+    image: 'unoCard blue card1'
 }
 
 const b2 = {
     id: 'b2',
     value: 2,
     color: 'blue',
-    image: '.unoCard.blue.card2'
+    image: 'unoCard blue card2'
 }
 
 const b3 = {
     id: 'b3',
     value: 3,
     color: 'blue',
-    image: '.unoCard.blue.card3'
+    image: 'unoCard blue card3'
 }
 
 const b4 = {
     id: 'b4',
     value: 4,
     color: 'blue',
-    image: '.unoCard.blue.card4'
+    image: 'unoCard blue card4'
 }
 
 const b5 = {
     id: 'b5',
     value: 5,
     color: 'blue',
-    image: '.unoCard.blue.card5'
+    image: 'unoCard blue card5'
 }
 
 const b6 = {
     id: 'b6',
     value: 6,
     color: 'blue',
-    image: '.unoCard.blue.card6'
+    image: 'unoCard blue card6'
 }
 
 const b7 = {
     id: 'b7',
     value: 7,
     color: 'blue',
-    image: '.unoCard.blue.card7'
+    image: 'unoCard blue card7'
 }
 
 const b8 = {
     id: 'b8',
     value: 8,
     color: 'blue',
-    image: '.unoCard.blue.card8'
+    image: 'unoCard blue card8'
 }
 
 const b9 = {
     id: 'b9',
     value: 9,
     color: 'blue',
-    image: '.unoCard.blue.card9'
+    image: 'unoCard blue card9'
 }
 
 const b_rev = {
     id: 'brev',
     value: 11,
     color: 'blue',
-    image: '.unoCard.blue.reverse'
+    image: 'unoCard blue reverse'
 }
 
 const b_draw2 = {
     id: 'bdraw2',
     value: 12,
     color: 'blue',
-    image: '.unoCard.blue.draw2'
+    image: 'unoCard blue draw2'
 }
 
 const b_skip = {
     id: 'bskip',
     value: 13,
     color: 'blue',
-    image: '.unoCard.blue.skip'
+    image: 'unoCard blue skip'
 }
 
 //Yellow card declarations
@@ -187,84 +187,84 @@ const y1 = {
     id: 'y1',
     value: 1,
     color: 'yellow',
-    image: '.unoCard.yellow.card1'
+    image: 'unoCard yellow card1'
 }
 
 const y2 = {
     id: 'y2',
     value: 2,
     color: 'yellow',
-    image: '.unoCard.yellow.card2'
+    image: 'unoCard yellow card2'
 }
 
 const y3 = {
     id: 'y3',
     value: 3,
     color: 'yellow',
-    image: '.unoCard.yellow.card3'
+    image: 'unoCard yellow card3'
 }
 
 const y4 = {
     id: 'y4',
     value: 4,
     color: 'yellow',
-    image: '.unoCard.yellow.card4'
+    image: 'unoCard yellow card4'
 }
 
 const y5 = {
     id: 'y5',
     value: 5,
     color: 'yellow',
-    image: '.unoCard.yellow.card5'
+    image: 'unoCard yellow card5'
 }
 
 const y6 = {
     id: 'y6',
     value: 6,
     color: 'yellow',
-    image: '.unoCard.yellow.card6'
+    image: 'unoCard yellow card6'
 }
 
 const y7 = {
     id: 'y7',
     value: 7,
     color: 'yellow',
-    image: '.unoCard.yellow.card7'
+    image: 'unoCard yellow card7'
 }
 
 const y8 = {
     id: 'y8',
     value: 8,
     color: 'yellow',
-    image: '.unoCard.yellow.card8'
+    image: 'unoCard yellow card8'
 }
 
 const y9 = {
     id: 'y9',
     value: 9,
     color: 'yellow',
-    image: '.unoCard.yellow.card9'
+    image: 'unoCard yellow card9'
 }
 
 const y_rev = {
     id: 'yrev',
     value: 11,
     color: 'yellow',
-    image: '.unoCard.yellow.reverse'
+    image: 'unoCard yellow reverse'
 }
 
 const y_draw2 = {
     id: 'ydraw2',
     value: 12,
     color: 'yellow',
-    image: '.unoCard.yellow.draw2'
+    image: 'unoCard yellow draw2'
 }
 
 const y_skip = {
     id: 'yskip',
     value: 13,
     color: 'yellow',
-    image: '.unoCard.yellow.skip'
+    image: 'unoCard yellow skip'
  }
 
 //Green card declarations
@@ -273,84 +273,84 @@ const g1 = {
     id: 'g1',
     value: 1,
     color: 'green',
-    image: '.unoCard.green.card1'
+    image: 'unoCard green card1'
 }
 
 const g2 = {
     id: 'g2',
     value: 2,
     color: 'green',
-    image: '.unoCard.green.card2'
+    image: 'unoCard green card2'
 }
 
 const g3 = {
     id: 'g3',
     value: 3,
     color: 'green',
-    image: '.unoCard.green.card3'
+    image: 'unoCard green card3'
 }
 
 const g4 = {
     id: 'g4',
     value: 4,
     color: 'green',
-    image: '.unoCard.green.card4'
+    image: 'unoCard green card4'
 }
 
 const g5 = {
     id: 'g5',
     value: 5,
     color: 'green',
-    image: '.unoCard.green.card5'
+    image: 'unoCard green card5'
 }
 
 const g6 = {
     id: 'g6',
     value: 6,
     color: 'green',
-    image: '.unoCard.green.card6'
+    image: 'unoCard green card6'
 }
 
 const g7 = {
     id: 'g7',
     value: 7,
     color: 'green',
-    image: '.unoCard.green.card7'
+    image: 'unoCard green card7'
 }
 
 const g8 = {
     id: 'g8',
     value: 8,
     color: 'green',
-    image: '.unoCard.green.card8'
+    image: 'unoCard green card8'
 }
 
 const g9 = {
     id: 'g9',
     value: 9,
     color: 'green',
-    image: '.unoCard.green.card9'
+    image: 'unoCard green card9'
 }
 
 const g_rev = {
     id: 'rrev',
     value: 11,
     color: 'red',
-    image: '.unoCard.red.reverse'
+    image: 'unoCard red reverse'
 }
 
 const g_draw2 = {
     id: 'gdraw2',
     value: 12,
     color: 'green',
-    image: '.unoCard.green.draw2'
+    image: 'unoCard green draw2'
 }
 
 const g_skip = {
     id: 'gskip',
     value: 13,
     color: 'green',
-    image: '.unoCard.green.skip'
+    image: 'unoCard green skip'
 }
 
 // Special card declarations
@@ -359,14 +359,14 @@ const draw4 = {
     id: 'draw4',
     value: 14,
     color: '',
-    image: '.unoCard.green.draw4'
+    image: 'unoCard draw4'
 }
 
 const wild = {
     id: 'wild',
     value: 15,
     color: '',
-    image: '.unoCard.green.wild'
+    image: 'unoCard wild'
 }
 
 // export default allCards;
