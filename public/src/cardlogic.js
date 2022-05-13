5964c92d275af2772ccfa735b1f96ff957998f65
Chi Tran <ttranpham@mail.sfsu.edu>
2022-05-12 23:06:43 -0700

    added dynamic render cards

---
+++ public/src/cardlogic.js
@@ -0,0 +1,17 @@
+// import { wild } from './cards.js'
+// console.log(wild);
+import { allCards } from './cards.js'
+// allCards
+var player2Deck = document.getElementsByClassName("playerTwo playerDeck")[0];
+
+const mycard = document.createElement("div");
+
+// mydeck.forEach( () => {
+//     mycard.className = "unoCard red reverse";
+//     player2Deck.appendChild(mycard);
+//
+// })
+
+mycard.className = allCards.r1.image;
+player2Deck.appendChild(mycard);
+// document.getElementsByClassName("playerTwo playerDeck")[0].appendChild(mycard);
