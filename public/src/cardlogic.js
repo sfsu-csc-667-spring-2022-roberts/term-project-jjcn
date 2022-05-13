027c0eb5fa0cfd27aaa7bbc04de3d889a7a4bb1a
Chi Tran <ttranpham@mail.sfsu.edu>
2022-05-13 15:50:49 -0700

    added click listener to play and draw cards

--- public/src/cardlogic.js
+++ public/src/cardlogic.js
@@ -1,18 +1,42 @@
-// import { wild } from './cards.js'
-// console.log(wild);
-import { allCards } from './cards.js'
+import { allCards } from './cards.js';
 
-// allCards
-var player2Deck = document.getElementsByClassName("playerTwo playerDeck")[0];
+// renders the card on the document
+function renderCard(card, deck) {
+    const mycard = document.createElement("div");
+    mycard.className = card.image;
+    mycard.onclick = function() {playCard(card)};
+    deck.appendChild(mycard);
+    return deck;
+}
 
-const mycard = document.createElement("div");
+// pop the pullDeck and adds card to playerDeck
+function drawCard (playerDeck) {
+    const topCard = drawDeck_arr.pop();
+    renderCard(topCard, player1Deck_div);
+}
 
-// mydeck.forEach( () => {
-//     mycard.className = "unoCard red reverse";
-//     player2Deck.appendChild(mycard);
-//
-// })
+// remove card from player deck and display in throw deck
+function playCard(card) {
+    var currentCards = player1Deck_div.childNodes;
+//     console.log(typeof currentCards, currentCards, currentCards[0].className, card, card.image);
+    var card_divs = [...currentCards].filter(element => element.className == card.image);
+//     console.log("here", card_divs[0]);
+    card_divs[0].remove();
+    var throwDeck = document.getElementsByClassName("throwDeck")[0];
+    throwDeck.className = "throwDeck " + card.image;
+}
 
-mycard.className = allCards.r1.image;
-player2Deck.appendChild(mycard);
-// document.getElementsByClassName("playerTwo playerDeck")[0].appendChild(mycard);
+// debug
+
+var drawDeck_arr = Object.values(allCards);
+var drawDeck_div = document.getElementsByClassName("pullDeck")[0];
+
+var player1Deck_div = document.getElementsByClassName("playerOne playerDeck")[0];
+var player1Deck_arr = [allCards.r1, allCards.b7, allCards.g_rev, allCards.y_skip];
+
+drawDeck_div.onclick = function() { drawCard(player1Deck_arr) };
+
+player1Deck_arr.forEach(element => renderCard(element, player1Deck_div));
+
+
+
